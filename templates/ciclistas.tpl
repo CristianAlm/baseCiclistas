5cb26ad3280692d891e84dbdac5d2a75eeb53d8b
Cristian Almiron <cristianalmiron1988@gmail.com>
2020-10-15 07:49:58 -0300

    Listo de pulir una parte del proyecto en smarty y funciona

--- templates/ciclistas.tpl
+++ templates/ciclistas.tpl
@@ -4,33 +4,23 @@
     <form action="paraEditar/{$idCiclista}" method="post">
         <div class="form-group">
             <label for="corredor">Corredor</label>
-            <input class="form-control" id="corredor" name="corredor" aria-describedby="emailHelp" value={$nuevoCorredor}>
+            <input class="form-control" id="corredor" name="corredor" aria-describedby="emailHelp" value="{$nuevoCorredor}">
             <small id="emailHelp" class="form-text text-muted">Nombre y apellido del corredor</small>
         </div>
         <div class="form-group">
             <label for="equipo">Equipo</label>
-            <input class="form-control" id="equipo" name="equipo" value={$nuevoEquipo}>
+            <input class="form-control" id="equipo" name="equipo" value="{$nuevoEquipo}">
         </div>
         <div class="form-group">
                     <label for="especialidad">Especialidad</label>
-                    <input class="form-control" id="especialidad" name="especialidad" aria-describedby="emailHelp" value={$nuevaEspecialidad}>
+                    <input class="form-control" id="especialidad" name="especialidad" aria-describedby="emailHelp" value="{$nuevaEspecialidad}">
                     <small id="emailHelp" class="form-text text-muted">Recuerde que las especialidad son 6: vuelta por etapas, escalador, sprinter, clasicomano, vueltas de 1 semana y gregario</small>
                 </div>
         <div class="form-group">
             <label for="edad">Edad</label>
-            <input class="form-control" id="edad"  name="edad" value={$nuevaEdad}>
+            <input class="form-control" id="edad"  name="edad" value="{$nuevaEdad}">
         </div>
         
-        <div class="form-group">
-                <label for="input_division">Categoria del equipo:</label>
-                <input class="form-control" id="catEquipos" name="division" aria-describedby="emailHelp">
-                <small id="emailHelp" class="form-text text-muted">Recuerde que las categorias son 3: Word Tour, Continental Pro y Continental</small>
-        </div>
-
-        <div class="form-group">
-            <label for="pais">Pais Equipo</label>
-            <input class="form-control" id="pais"  name="pais">
-        </div>
         <button type="submit" class="btn btn-primary">Editar</button>
         </form>
 </div>
--- templates_c/ebec73380e85b342fad01dc75b5c21079831f4c9_0.file.ciclistas.tpl.php
+++ templates_c/ebec73380e85b342fad01dc75b5c21079831f4c9_0.file.ciclistas.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-10-15 12:36:10
+/* Smarty version 3.1.34-dev-7, created on 2020-10-15 12:48:53
   from 'C:\xampp\htdocs\baseCiclistas\templates\ciclistas.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5f88261a2318d6_54851054',
+  'unifunc' => 'content_5f8829151c9a98_67446391',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ebec73380e85b342fad01dc75b5c21079831f4c9' => 
     array (
       0 => 'C:\\xampp\\htdocs\\baseCiclistas\\templates\\ciclistas.tpl',
-      1 => 1602758144,
+      1 => 1602758913,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_5f88261a2318d6_54851054 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f8829151c9a98_67446391 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -31,37 +31,27 @@ $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_sm
 " method="post">
         <div class="form-group">
             <label for="corredor">Corredor</label>
-            <input class="form-control" id="corredor" name="corredor" aria-describedby="emailHelp" value=<?php echo $_smarty_tpl->tpl_vars['nuevoCorredor']->value;?>
->
+            <input class="form-control" id="corredor" name="corredor" aria-describedby="emailHelp" value="<?php echo $_smarty_tpl->tpl_vars['nuevoCorredor']->value;?>
+">
             <small id="emailHelp" class="form-text text-muted">Nombre y apellido del corredor</small>
         </div>
         <div class="form-group">
             <label for="equipo">Equipo</label>
-            <input class="form-control" id="equipo" name="equipo" value=<?php echo $_smarty_tpl->tpl_vars['nuevoEquipo']->value;?>
->
+            <input class="form-control" id="equipo" name="equipo" value="<?php echo $_smarty_tpl->tpl_vars['nuevoEquipo']->value;?>
+">
         </div>
         <div class="form-group">
                     <label for="especialidad">Especialidad</label>
-                    <input class="form-control" id="especialidad" name="especialidad" aria-describedby="emailHelp" value=<?php echo $_smarty_tpl->tpl_vars['nuevaEspecialidad']->value;?>
->
+                    <input class="form-control" id="especialidad" name="especialidad" aria-describedby="emailHelp" value="<?php echo $_smarty_tpl->tpl_vars['nuevaEspecialidad']->value;?>
+">
                     <small id="emailHelp" class="form-text text-muted">Recuerde que las especialidad son 6: vuelta por etapas, escalador, sprinter, clasicomano, vueltas de 1 semana y gregario</small>
                 </div>
         <div class="form-group">
             <label for="edad">Edad</label>
-            <input class="form-control" id="edad"  name="edad" value=<?php echo $_smarty_tpl->tpl_vars['nuevaEdad']->value;?>
->
+            <input class="form-control" id="edad"  name="edad" value="<?php echo $_smarty_tpl->tpl_vars['nuevaEdad']->value;?>
+">
         </div>
         
-        <div class="form-group">
-                <label for="input_division">Categoria del equipo:</label>
-                <input class="form-control" id="catEquipos" name="division" aria-describedby="emailHelp">
-                <small id="emailHelp" class="form-text text-muted">Recuerde que las categorias son 3: Word Tour, Continental Pro y Continental</small>
-        </div>
-
-        <div class="form-group">
-            <label for="pais">Pais Equipo</label>
-            <input class="form-control" id="pais"  name="pais">
-        </div>
         <button type="submit" class="btn btn-primary">Editar</button>
         </form>
 </div>
